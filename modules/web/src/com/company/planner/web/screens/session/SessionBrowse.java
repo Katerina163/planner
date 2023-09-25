4a8d53ae36f4c4d2f35a0c925ef826d6a38cd098
Катерина Нефедова <nefedova@smarts.ru>
2023-09-25 10:07:29 +0400

    Добавление бизнес-логики

--- modules/web/src/com/company/planner/web/screens/session/SessionBrowse.java
+++ modules/web/src/com/company/planner/web/screens/session/SessionBrowse.java
@@ -2,11 +2,37 @@ package com.company.planner.web.screens.session;
 
 
 import com.company.planner.entity.Session;
+import com.company.planner.service.SessionService;
+import com.haulmont.cuba.gui.components.Calendar;
+import com.haulmont.cuba.gui.components.EditorScreenFacet;
+import com.haulmont.cuba.gui.model.CollectionContainer;
 import com.haulmont.cuba.gui.screen.*;
+
+import javax.inject.Inject;
+import java.time.LocalDateTime;
 
 @UiController("planner_Session.browse")
 @UiDescriptor("session-browse.xml")
 @LookupComponent("sessionsTable")
 @LoadDataBeforeShow
 public class SessionBrowse extends StandardLookup<Session> {
+
+    @Inject
+    private SessionService sessionService;
+    @Inject
+    private CollectionContainer<Session> sessionsDc;
+    @Inject
+    private EditorScreenFacet<Session, SessionEdit> sessionEditDialog;
+
+    @Subscribe("sessionsCalendar")
+    private void onSessionsCalendarCalendarEventClick(Calendar.CalendarEventClickEvent<LocalDateTime> event) {
+        sessionEditDialog.setEntityProvider(() -> (Session) event.getEntity());
+        sessionEditDialog.show();
+    }
+
+    @Subscribe("sessionsCalendar")
+    private void onSessionsCalendarCalendarEventMove(Calendar.CalendarEventMoveEvent<LocalDateTime> event) {
+        Session session = sessionService.rescheduleSession((Session) event.getEntity(), event.getNewStart());
+        sessionsDc.replaceItem(session);
+    }
 }
